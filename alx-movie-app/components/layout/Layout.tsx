19cb6688e0f9c7091fad2e8cc6fe30522dfb40bb
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2025-12-04 12:16:09 +0100

    Update site

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { ComponentProps } from "@/interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
 
-const Layout = () => {
+const Layout: React.FC = ({ children }) => {
   return (
-    <div>
-      <h1>LAYOUT</h1>
-    </div>
+    <>
+      <Header />
+      <main className="min-h-screen">({children})</main>
+      <Footer />
+    </>
   );
 };
 
