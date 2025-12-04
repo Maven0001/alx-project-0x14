085fcf85a3757fdfddc60b354753bb8c8feaf90e
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2025-12-04 12:54:11 +0100

    Updating the app

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // next.config.js
+
+  images: {
+    domains: ["m.media-amazon.com"], // ✅ allow Amazon media CDN
+  },
 };
 
 export default nextConfig;
